09d6fed813f8ef248ea44227b6921c8b2c8c3108
wetarorg <tarframework@gmail.com>
2024-10-15 18:18:06 +0530

    tar1

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  vite: {
+    ssr: {
+      noExternal: ['tinybase'],
+    },
+  },
+});
